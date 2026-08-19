5feeb003af9e5f78ada17c9c90df645300aab922
Pardhav Maradani <pardhav.maradani@gmail.com>
2026-08-19 11:09:23 +0530

    - Warm the uv cache during build, so uvx at runtime is quicker - Download the latest state file from repo at runtime (with fallback)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,18 @@
 
 cd /workdir/simulation
 ./run.sh > gromacs_output.log 2>&1 &
-cd
+cd /workdir/mdadash
+
+STATE_FILE="state.json"
+TMP_STATE_FILE="state.json.tmp"
+RAW_STATE_URL="https://raw.githubusercontent.com/PardhavMaradani/mdadash-demo/refs/heads/main/docker/mdadash/state.json"
+
+if curl -sS -fL "$RAW_STATE_URL" -o "$TMP_STATE_FILE"; then
+    mv "$TMP_STATE_FILE" "$STATE_FILE"
+else
+    echo "WARNING: Failed to download latest state.json from repo."
+fi
+
 uvx mdadash --topology=/workdir/simulation/input/start.gro \
     --trajectory imd://localhost:8889 \
     --dashboard-host=0.0.0.0 \
